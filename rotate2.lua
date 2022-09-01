27cb4d8b8a9c88374e8eb83803ae08f6f492c60d
霄雄 邢 <xingxiaoxiong@gmail.com>
2022-09-01 12:34:22 +0900

    Update

--- rotate2.lua
+++ rotate2.lua
@@ -2,6 +2,6 @@ test_actor.OnActorBeginOverlap = function(me, other)
     print('rotate2')
 end
 
-test_actor.ReceiveTick = function(me, delta)
-    print('hello')
+test_actor.Tick = function(me, delta)
+    me.K2_AddActorLocalRotation({Yaw=10 * delta, Pitch=0, Roll=0})
 end
